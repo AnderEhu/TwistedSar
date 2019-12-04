2718bc21c56be1a2084f0704fa3575849574f713
Ander <anderalonsogarcia@gmail.com>
2019-12-04 18:45:34 +0100

    add changes

--- serv_chat.py
+++ serv_chat.py
@@ -4,6 +4,7 @@ from twisted.protocols.basic import LineReceiver
 from twisted.internet.protocol import Factory
 from twisted.internet import reactor
 
+
 MAX_USERS = 100
 MAX_MSG_LENGTH = 255
 MAX_USER_LENGTH = 16
@@ -16,20 +17,71 @@ class ChatProtocol(LineReceiver):
 
     def connectionMade(self):
         #A COMPLETAR
+        users = ""
+        for user in self.factory.users:
+            users+=user
+            users+=" "
 
+        users = users[0:(len(users)-1)]
+        if len(users) == MAX_USERS:
+            connectionLost(self, 1)
+        else:
+            self.sendLine( "FTR0 0 0 0".encode( "utf-8" ) )
+            self.sendLine( ("USR{}".format( users)).encode( "utf-8" ) )
+
+        
     def connectionLost(self, reason):
+        del self.factory.users[self.name] #no funciona
+        for i in self.factory.users:
+            user = self.factory.users[i]
+            user.sendLine(("OUT{}".format(user.name)).encode("utf-8"))
         #A COMPLETAR
-
     def lineReceived(self, line):
-        #A COMPLETAR
+        mensage = line.decode("utf-8")
+
+        if mensage.startswith("NME"):
+            userName = mensage[3:]
+            #Si tienes caracteres prohibidos
+            if " " in userName:
+                self.sendLine( ("-{}".format( 2 )).encode( "utf-8" ) )
+            #Si el nombre es demasiado largo
+            elif len(userName) > MAX_USER_LENGTH:
+                self.sendLine( ("-{}".format( 3 )).encode( "utf-8" ) )
+
+            #Si el nombre ya existe
+            elif userName in self.factory.users:
+                self.sendLine( ("-{}".format( 4 )).encode( "utf-8" ) )
+
+            else:
+                #Enviar a todos los usuarios que ha entrado un usuario nuevo
+                for i in self.factory.users:
+                    self.factory.users[i].sendLine(("INN{}".format(userName)).encode("utf-8"))
+                self.factory.users[userName] = self
+                self.name = userName
+                self.sendLine("+".encode( "utf-8"))
+
+        if mensage.startswith("MSG"):
+            mensage = mensage[3:]
+            #Si el mensaje es demasiado largo
+            if len(mensage) > MAX_MSG_LENGTH:
+                self.sendLine( ("-{}".format( 5 )).encode( "utf-8" ) )
+            else:
+                self.sendLine("+".encode( "utf-8"))
+                for i in self.factory.users:
+                    if self.name not in self.factory.users[i].name:
+                        self.factory.users[i].sendLine(("MSG{}{}".format(self.name,mensage)).encode("utf-8"))
+
+                
+
 
 class ChatFactory(Factory):
     def __init__(self):
         self.users = {}
 
     def buildProtocol(self, addr):
+         
         return ChatProtocol(self)
 
 if __name__ == "__main__":
-	reactor.listenTCP(PORT, ChatFactory())
-	reactor.run()
+    reactor.listenTCP(PORT, ChatFactory())
+    reactor.run()
